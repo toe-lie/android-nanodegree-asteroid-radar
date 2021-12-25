38a01da56e21ce651cce8da0dfcb5e07464b07ad
toelie <toelie.cumlm.cs@gmail.com>
2021-12-25 16:28:36 +0630

    Initial commit

---
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApp.kt
@@ -0,0 +1,8 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AsteroidRadarApp: Application() {
+}
---
+++ app/src/main/java/com/udacity/asteroidradar/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.udacity.asteroidradar.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
