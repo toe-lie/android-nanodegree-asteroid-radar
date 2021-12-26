252c7186461430531bf6dbbe7b071fe0565eaee8
toelie <toelie.cumlm.cs@gmail.com>
2021-12-26 13:52:51 +0630

    Sort asteroid list by close_approach_date in dao

--- app/src/main/java/com/udacity/asteroidradar/data/source/local/database/daos/AsteroidDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/data/source/local/database/daos/AsteroidDao.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 abstract class AsteroidDao {
 
-  @Query("SELECT * FROM asteroid")
+  @Query("SELECT * FROM asteroid ORDER BY date(close_approach_date) ASC")
   abstract fun getAllAsteroids(): Flow<List<CacheAsteroid>>
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
