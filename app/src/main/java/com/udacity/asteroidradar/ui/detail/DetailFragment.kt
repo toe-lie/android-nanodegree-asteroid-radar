685cf9e24f3b91e59308ec508331d8316a4cfea8
toelie <toelie.cumlm.cs@gmail.com>
2021-12-27 10:26:04 +0630

    Replace AstronomicalUnitExplanation AppCompat alert dialog with Material alert dialog

--- app/src/main/java/com/udacity/asteroidradar/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/detail/DetailFragment.kt
@@ -1,40 +1,40 @@
 package com.udacity.asteroidradar.ui.detail
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.DetailFragmentBinding
 import com.udacity.asteroidradar.util.autoCleared
 
 class DetailFragment : Fragment() {
 
-    private var binding by autoCleared<DetailFragmentBinding>()
+  private var binding by autoCleared<DetailFragmentBinding>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        binding = DetailFragmentBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+  ): View {
+    binding = DetailFragmentBinding.inflate(inflater)
+    binding.lifecycleOwner = this
 
-        val asteroid = DetailFragmentArgs.fromBundle(arguments!!).selectedAsteroid
+    val asteroid = DetailFragmentArgs.fromBundle(arguments!!).selectedAsteroid
 
-        binding.asteroid = asteroid
+    binding.asteroid = asteroid
 
-        binding.helpButton.setOnClickListener {
-            displayAstronomicalUnitExplanationDialog()
-        }
+    binding.helpButton.setOnClickListener { displayAstronomicalUnitExplanationDialog() }
 
-        return binding.root
-    }
+    return binding.root
+  }
 
-    private fun displayAstronomicalUnitExplanationDialog() {
-        val builder = AlertDialog.Builder(activity!!)
+  private fun displayAstronomicalUnitExplanationDialog() {
+        MaterialAlertDialogBuilder(requireContext(), R.style.ThemeOverlay_MaterialComponents_Light)
             .setMessage(getString(R.string.astronomica_unit_explanation))
             .setPositiveButton(android.R.string.ok, null)
-        builder.create().show()
-    }
+            .show()
+  }
 }
